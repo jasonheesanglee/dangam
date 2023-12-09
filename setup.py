c5170884ecfc789d2f75251736a304857a765c9b
jasonheesanglee <jason.heesang.lee96@gmail.com>
2023-12-10 07:52:25 +0900

    config : updated setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,9 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3',
+
+        'Programming Language :: Python :: 3.10',
+
         'Operating System :: OS Independent',  # Use specific OS classifiers if your package is OS-dependent
 
     ]
