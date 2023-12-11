c288024037c096be410ebd7c4b66c76cd0266274
jasonheesanglee <jason.heesang.lee96@gmail.com>
2023-12-11 16:18:18 +0900

    feat: fixed error in noun_emotions.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='DanGam',
-    version='0.0.121',
+    version='0.0.122',
     packages=find_packages(),
     description='A brief description of your package',
     long_description=open('README.md').read(),
